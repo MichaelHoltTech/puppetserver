d8fb1b04e90d52f3ee42ea75f22efbfa613028de
Michael Holt <mholt@reallifechurch.org>
2015-11-17 10:24:26 -0800

    Run puppet agent 5 minutes after boot and every 30 minutes

--- my_init.d/05_run_puppet.sh
+++ my_init.d/05_run_puppet.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-puppet agent -t
+
+# load environment variables
+source /etc/container_environment.sh
+
+# default cron setting is every 30 minutes
+test -z "$PUPPET_AGENT_CRON" && export PUPPET_AGENT_CRON="0,30 * * * *"
+
+read -d '' cronscript <<EOF
+# Runs the Puppet Agent on a Schedule!
+SHELL=/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin
+@reboot root sleep 300 && /sbin/run-puppet-agent
+$PUPPET_AGENT_CRON root /sbin/run-puppet-agent
+EOF
+
+echo "$cronscript" > /etc/cron.d/puppet && chmod +x /etc/cron.d/puppet

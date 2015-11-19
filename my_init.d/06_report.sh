da06a46e2492751ce9941e8ca8f50e08dabc3882
Michael Holt <mholttech@users.noreply.github.com>
2015-11-18 23:29:01 -0800

    update the report output

--- my_init.d/06_report.sh
+++ my_init.d/06_report.sh
@@ -5,13 +5,20 @@ echo "Configuration Complete"
 echo ""
 echo "Ensure that you clone the example core_repo and make it PRIVATE using bitbucket"
 echo ""
-echo "Add the following PublicKey to allow the Puppetserver to pull your Core_Repo"
-echo ""
-cat /root/.ssh/id_rsa.pub
-echo ""
+if [ -f /root/.ssh/id_rsa.pub ]; then
+  echo "Add the following PublicKey to allow the Puppetserver to pull your Core_Repo"
+  echo ""
+  cat /root/.ssh/id_rsa.pub
+  echo ""
+else
+  echo "You have specified an existing Private Key.  Please ensure you add the public key"
+  echo "to your core_repo deployment keys to allow R10K to work"
+  echo ""
+fi
 if [ -f /root/.new_rsa ]; then
   echo "It appears that the SSH Key has been regenerated.  Please verify that your core_repo"
   echo "has the correct key configured for R10K to work"
+  echo ""
   rm /root/.new_rsa
 fi
 echo "=========="

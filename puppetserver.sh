dda6e01aaa03d74d5c41b01713beac3893dcfa67
Michael Holt <mholt@reallifechurch.org>
2015-12-02 21:28:02 -0800

    output puppetserver log to syslog

--- puppetserver.sh
+++ puppetserver.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
-exec /usr/bin/puppetserver foreground >> /var/log/puppetserver.log 2>&1
+exec /usr/bin/puppetserver foreground >> /var/log/syslog
